1f9266d997aa061ae8ca1e403a9696e0929f4da2
unknown <ritachetadas@gmail.com>
2021-04-23 11:33:54 +0530

    Assignment 2

---
+++ src/java_assignments/Java_Assignment_2/BankAcct.java
@@ -0,0 +1,85 @@
+import java.util.*;
+public class BankAcct{
+    String accountNum;
+    double amount;
+    static int last_id=0;
+    static double interest_rate=0;
+    double interest;
+    BankAcct()
+    {
+        accountNum=null;
+    }
+    BankAcct(Scanner s)
+    {
+        last_id+=1;
+        Date d=new Date();
+        int y = d.getYear() + 1900;
+        String sr = String.format("%03d", last_id);
+        accountNum = new String(y + sr);
+        System.out.println("Enter the amount:-");
+        amount = s.nextDouble();
+        interest = amount*interest_rate/100;
+        amount+=interest;
+        s.nextLine();
+        System.out.println("Account Successfully created!");
+        System.out.println("Your Bank Account Number:-"+accountNum);
+    }
+    static void initialize_interest(Scanner s)
+    {
+        System.out.println("enter the initial interset rate:");
+        interest_rate=s.nextDouble();
+        s.nextLine();
+    }
+    static void setInterest(Scanner s)
+    {
+        System.out.println("enter the interset rate to change:");
+        interest_rate = s.nextDouble();
+        s.nextLine();
+    }
+    static void show_interest_rate()
+    {
+        System.out.println("Interest rate:-"+interest_rate);
+    }
+    double get_balance()
+    {
+        return amount;
+    }
+    double get_interest()
+    {
+        return interest;
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int ch;
+        BankAcct b;//=new BankAcct();
+        while(true)
+        {
+            System.out.println("\n\t1. Create account\n\t2. Intialize Interest rate\n\t3. Change Interest rate\n\t4. Display Interest rate\n\t5. Get Balance\n\t6. Calculate Interest\n\t7. Exit");
+            ch=s.nextInt();
+            s.nextLine();
+            switch(ch)
+            {
+                case 1:b=new BankAcct(s);
+                case 2:BankAcct.initialize_interest(s);
+                case 3:BankAcct.setInterest(s);
+                case 4:BankAcct.show_interest_rate();
+                case 5:
+                try{
+                    b.get_balance();
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Create a account fisrt please!!");
+                }
+                case 6:
+                    try {
+                        b.get_interest();
+                    } catch (Exception e) {
+                        System.out.println("Create a account fisrt please!!");
+                    }
+                default:System.exit(0);
+            }
+        }
+    }
+}
